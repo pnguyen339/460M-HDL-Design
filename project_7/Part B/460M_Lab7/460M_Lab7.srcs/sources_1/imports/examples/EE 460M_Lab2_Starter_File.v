0c09e2829584b847a5b5f3ca9cba9db56c2202f2
Erick Shepherd <erickshepherd@utexas.edu>
2018-04-24 21:27:06 -0500

    Part B set up
    Just need to add the new instructions and convert the test program to machine code

---
+++ project_7/Part B/460M_Lab7/460M_Lab7.srcs/sources_1/imports/examples/EE 460M_Lab2_Starter_File.v
@@ -0,0 +1,172 @@
+/* Example 1 */
+module simpleDivider(clk100Mhz, slowClk);
+  input clk100Mhz; //fast clock
+  output slowClk; //slow clock
+
+  reg[27:0] counter;
+  assign slowClk= counter[27];  //(2^27 / 100E6) = 1.34seconds
+
+  initial begin
+    counter = 0;
+  end
+
+  always @ (posedge clk100Mhz)
+  begin
+    counter <= counter + 1; //increment the counter every 10ns (1/100 Mhz) cycle.
+  end
+
+endmodule
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+/* Example 2 */
+module complexDivider(clk100Mhz, slowClk);
+  input clk100Mhz; //fast clock
+  output reg slowClk; //slow clock
+
+  reg[27:0] counter;
+
+  initial begin
+    counter = 0;
+    slowClk = 0;
+  end
+
+  always @ (posedge clk100Mhz)
+  begin
+    if(counter == 50000000) begin
+      counter <= 1;
+      slowClk <= ~slowClk;
+    end
+    else begin
+      counter <= counter + 1;
+    end
+  end
+
+endmodule
+
+
+module clockDivider_1Hz(clk100Mhz, slowClk1);
+  input clk100Mhz; //fast clock
+  output reg slowClk1; //slow clock
+
+  reg[27:0] counter;
+
+  initial begin
+    counter = 0;
+    slowClk1 = 0;
+  end
+
+  always @ (posedge clk100Mhz)
+  begin
+    if(counter == 50000000) begin
+      counter <= 1;
+      slowClk1 <= ~slowClk1;
+    end
+    else begin
+      counter <= counter + 1;
+    end
+  end
+
+endmodule
+
+module clockDivider_2Hz(clk100Mhz, slowClk2);
+  input clk100Mhz; //fast clock
+  output reg slowClk2; //slow clock
+
+  reg[27:0] counter;
+
+  initial begin
+    counter = 0;
+    slowClk2 = 0;
+  end
+
+  always @ (posedge clk100Mhz)
+  begin
+    if(counter == 100000000) begin
+      counter <= 1;
+      slowClk2 <= ~slowClk2;
+    end
+    else begin
+      counter <= counter + 1;
+    end
+  end
+endmodule
+
+module clockDivider_20ms(clk100Mhz, slowClk3);
+  input clk100Mhz; //fast clock
+  output reg slowClk3; //slow clock
+
+  reg[27:0] counter;
+
+  initial begin
+    counter = 0;
+    slowClk3 = 0;
+  end
+
+  always @ (posedge clk100Mhz)
+  begin
+    if(counter == 1000000) begin
+      counter <= 1;
+      slowClk3 <= ~slowClk3;
+    end
+    else begin
+      counter <= counter + 1;
+    end
+  end
+endmodule
+
+module clockDivider_1s(clk100Mhz, slowClk3, reset, start);
+  input clk100Mhz, start, reset; //fast clock
+  output reg slowClk3; //slow clock
+
+  reg[27:0] counter;
+
+  initial begin
+    counter <= 0;
+    slowClk3 <= 1;
+  end
+
+  always @ (posedge clk100Mhz)
+  begin
+    if(reset == 1 || start == 0) begin
+        counter <= 0;
+    end
+    else begin // start is 1
+      if(counter == 50000000) begin
+        counter <= 0;
+        slowClk3 <= ~slowClk3;
+      end
+      else begin
+        counter <= counter + 1;
+      end
+    end
+  end
+endmodule
+
+module clockDivider_16ms(clk100Mhz, slowClk3);
+  input clk100Mhz; //fast clock
+  output reg slowClk3; //slow clock
+
+  reg[27:0] counter;
+
+  initial begin
+    counter = 0;
+    slowClk3 = 0;
+  end
+
+  always @ (posedge clk100Mhz)
+  begin
+    if(counter == 200000) begin
+      counter <= 1;
+      slowClk3 <= ~slowClk3;
+    end
+    else begin
+      counter <= counter + 1;
+    end
+  end
+
+endmodule
---
+++ project_7/Part B/460M_Lab7/460M_Lab7.srcs/sources_1/imports/new/NumberDisplay.v
@@ -0,0 +1,152 @@
+
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/03/2018 08:14:27 PM
+// Design Name: 
+// Module Name: NumberDisplay
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module NumberDisplay(count,segment_dis, an, clk);
+    input wire [15:0] count;
+    input clk;
+    output reg [6:0] segment_dis;
+    output wire [3:0] an;
+    
+    reg [3:0] ans;
+    reg [6:0] first,second,third, fourth;
+    reg [3:0] st,nd,rd,th;
+
+    
+    initial begin
+        first <= 0;
+        second <= 0;
+        third <= 0;
+        fourth <= 0;
+        st <= 0;
+        nd <= 0;
+        rd <= 0;
+        th <= 0;
+        ans <= 4'b0000;
+    end
+    
+    always @(posedge clk) begin
+        ans = ans<<1;
+            
+        if(ans[3:0] == 4'b0000) begin
+            ans = 2'b0001;
+        end
+        
+        case(ans)
+            4'b0001: segment_dis <= first;
+            4'b0010: segment_dis <= second;
+            4'b0100: segment_dis <= third;
+            4'b1000: segment_dis <= fourth;
+        endcase;                 
+    end
+
+    assign an = ~ans;
+     
+    always @(count) begin
+        st <= count & 4'b1111;
+        nd <= (count >> 4) & 4'b1111;
+        rd <= (count >> 8) & 4'b1111;
+        th <= (count >> 12) & 4'b1111;
+        
+        case(st)
+            4'b0000: first <= ~7'b0111111;
+            4'b0001: first <= ~7'b0000110;
+            4'b0010: first <= ~7'b1011011;
+            4'b0011: first <= ~7'b1001111;
+            4'b0100: first <= ~7'b1100110;
+            4'b0101: first <= ~7'b1101101;
+            4'b0110: first <= ~7'b1111101;
+            4'b0111: first <= ~7'b0000111;
+            4'b1000: first <= ~7'b1111111;
+            4'b1001: first <= ~7'b1101111;
+            4'b1010: first <= ~7'b0001000; // _
+            4'b1011: first <= ~7'b1111100; // b
+            4'b1100: first <= ~7'b0111001; // C
+            4'b1101: first <= ~7'b1011110; // d
+            4'b1110: first <= ~7'b1111001; // E
+            4'b1111: first <= ~7'b1110001; // F
+            default: first <= ~7'b0000000;
+        endcase
+        
+        case(nd)
+            4'b0000: second <= ~7'b0111111;
+            4'b0001: second <= ~7'b0000110;
+            4'b0010: second <= ~7'b1011011;
+            4'b0011: second <= ~7'b1001111;
+            4'b0100: second <= ~7'b1100110;
+            4'b0101: second <= ~7'b1101101;
+            4'b0110: second <= ~7'b1111101;
+            4'b0111: second <= ~7'b0000111;
+            4'b1000: second <= ~7'b1111111;
+            4'b1001: second <= ~7'b1101111;
+            4'b1010: second <= ~7'b0001000; // _
+            4'b1011: second <= ~7'b1111100; // b
+            4'b1100: second <= ~7'b0111001; // C
+            4'b1101: second <= ~7'b1011110; // d
+            4'b1110: second <= ~7'b1111001; // E
+            4'b1111: second <= ~7'b1110001; // F
+            default: second <= ~7'b0000000;
+        endcase
+        
+        case(rd)
+            4'b0000: third <= ~7'b0111111;
+            4'b0001: third <= ~7'b0000110;
+            4'b0010: third <= ~7'b1011011;
+            4'b0011: third <= ~7'b1001111;
+            4'b0100: third <= ~7'b1100110;
+            4'b0101: third <= ~7'b1101101;
+            4'b0110: third <= ~7'b1111101;
+            4'b0111: third <= ~7'b0000111;
+            4'b1000: third <= ~7'b1111111;
+            4'b1001: third <= ~7'b1101111;
+            4'b1010: third <= ~7'b0001000; // _
+            4'b1011: third <= ~7'b1111100; // b
+            4'b1100: third <= ~7'b0111001; // C
+            4'b1101: third <= ~7'b1011110; // d
+            4'b1110: third <= ~7'b1111001; // E
+            4'b1111: third <= ~7'b1110001; // F
+            default: third <= ~7'b0000000;
+        endcase
+        
+        case(th)
+            4'b0000: fourth <= ~7'b0111111;
+            4'b0001: fourth <= ~7'b0000110;
+            4'b0010: fourth <= ~7'b1011011;
+            4'b0011: fourth <= ~7'b1001111;
+            4'b0100: fourth <= ~7'b1100110;
+            4'b0101: fourth <= ~7'b1101101;
+            4'b0110: fourth <= ~7'b1111101;
+            4'b0111: fourth <= ~7'b0000111;
+            4'b1000: fourth <= ~7'b1111111;
+            4'b1001: fourth <= ~7'b1101111;
+            4'b1010: fourth <= ~7'b0001000; // _
+            4'b1011: fourth <= ~7'b1111100; // b
+            4'b1100: fourth <= ~7'b0111001; // C
+            4'b1101: fourth <= ~7'b1011110; // d
+            4'b1110: fourth <= ~7'b1111001; // E
+            4'b1111: fourth <= ~7'b1110001; // F
+            default: fourth <= ~7'b0000000;
+        endcase  
+            
+    end
+    
+    
+endmodule
